e5a65fb6a6306ba1a9a7e1802b451d004ca20abc
Yurij <nabor_2008@i.ua>
2017-01-24 22:47:26 +0200

    pagination

---
+++ pagination.php
@@ -0,0 +1,47 @@
+<?php
+$rowcount = 0;
+$counts = $dbh->query("SELECT COUNT('id') FROM testsfdrupal.article WHERE status = 1")->fetch(PDO::FETCH_ASSOC);
+//рахуємо кількість записів в БД
+foreach ($counts as $count) {
+    $rowcount = $count;
+}
+echo "Кількість записів з відповідними параметрами в БД: ";
+echo $rowcount;
+// приймаємо параметр із адресної строки
+$pagination = $_GET['pagination'];
+//задаємо параметр якщо пустий для відображення на головній сторінці
+if (empty($pagination)) {
+    $pagination = 1;
+}
+//умови для відображення пагінації
+if ($rowcount > 0) {
+    echo "<hr>";
+    echo "<a href = index.php?pagination=1>";
+    echo "1 - 10";
+    echo "</a>";
+}
+if ($rowcount > 10) {
+    echo " | ";
+    echo "<a href = index.php?pagination=2>";
+    echo "11 - 20";
+    echo "</a>";
+}
+if ($rowcount > 20) {
+    echo " | ";
+    echo "<a href = index.php?pagination=3>";
+    echo "21 - 30";
+    echo "</a>";
+}
+//задаємо значення для $a яка відправляється в зарос до БД
+switch ($pagination) {
+    case 1:
+        $a = 0;
+        break;
+    case 2:
+        $a = 10;
+        break;
+    case 3:
+        $a = 20;
+        break;
+}
+
--- viewarticles.php
+++ viewarticles.php
@@ -1,33 +1,9 @@
 <article class="col-lg-8">
     <?php
-    $rowcount = 0;
-    $counts = $dbh->query("SELECT COUNT('id') FROM testsfdrupal.article WHERE status = 1")->fetch(PDO::FETCH_ASSOC);
-
-    //рахуємо кількість записів в БД
-    foreach ($counts as $count) {
-        $rowcount = $count;
-    }
-    echo "Кількість записів з відповідними параметрами в БД: ";
-    echo $rowcount;
-
-    $pagination = $_GET['pagination'];
-
-    echo "<hr>";
-    echo "<a href = index.php?pagination=1>";
-    echo "1 - 10";
-    echo "</a>";
-    echo " | ";
-    echo "<a href = index.php?pagination=2>";
-    echo "11 - 20";
-    echo "</a>";
-
-    $a = 1;
-    $b = 40;
-
+    include "pagination.php";
     $i = 0;
-    $select = $dbh->query("SELECT id, category, tiser, date, status, articlename FROM testsfdrupal.article WHERE status = 1 AND id BETWEEN '$a' AND '$b' ORDER BY date");
-
-
+    //    $select = $dbh->query("SELECT id, category, tiser, date, status, articlename FROM testsfdrupal.article WHERE status = 1 AND id BETWEEN '$a' AND '$b' ORDER BY date");
+    $select = $dbh->query("SELECT id, category, tiser, date, status, articlename FROM testsfdrupal.article WHERE status = 1 LIMIT $a,10");
     // отримуємо статті
     foreach ($select as $article) {
         echo "<hr>";
@@ -47,8 +23,10 @@
         echo $article['status'];
         $i++;
     }
-
-
+    if ($i == 0) {
+        echo "<hr>";
+        echo "Немає записів на сторінці!";
+    }
     echo "<hr>";
     echo "Кількість записів на сторінці: ";
     echo $i;
